e53cfc656de9b50cf48ff297b4901679c566796e
tkovs <tkovs@outlook.com>
2017-10-25 00:02:32 -0300

    Namespace para simplificação do código

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
+use \App\Task;
+
 Route::get('/tasks', function () {
-	$tasks = \App\Task::all();
+	$tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$task = \App\Task::find($id);
+	$task = Task::find($id);
 
 	return view('tasks.show', compact('task'));
 });
