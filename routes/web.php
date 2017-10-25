dd9237b998b2c13b4cc0b1731519950667a36796
tkovs <tkovs@outlook.com>
2017-10-24 23:49:58 -0300

    Código mais limpo com uso de Model (php artisan make:model)

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,13 @@
 */
 
 Route::get('/tasks', function () {
-	$tasks = DB::table('tasks')->get();
+	$tasks = \App\Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$task = DB::table('tasks')->find($id);
+	$task = \App\Task::find($id);
 
 	return view('tasks.show', compact('task'));
 });
